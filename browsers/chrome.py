5b6e101e214300b06a4045f1ad31ffac02a3149a
Nikolay Despotov <85335497+NVDespotov20@users.noreply.github.com>
2023-05-23 12:56:03 +0300

    Update driver location

--- browsers/chrome.py
+++ browsers/chrome.py
@@ -5,7 +5,7 @@ def get_driver():
     chrome_options = Options()
     chrome_options.add_argument("--incognito")
 
-    chromedriver_path = "./chromedriver"
+    chromedriver_path = "./drivers/chromedriver"
 
     driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
     return driver
